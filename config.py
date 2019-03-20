82e65f7a6c99992b1a7b2bf1bae1e9b07f6a669b
MikuShare <2572623826@qq.com>
2019-03-20 14:36:49 +0800

    添加打码平台帐号开关

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
-#填写你的贴吧帐号密码
+# 填写你的贴吧帐号密码
 username = ''
 passwd = ''
+# 填写你的打码平台帐号密码,如没有请留空,密码可以为32位加密后的MD5
+rk_captcha = False # 开启打码为True,关闭打码为False,开启打码后务必填写帐号密码。
+username_rk = ''
+passwd_rk = ''
