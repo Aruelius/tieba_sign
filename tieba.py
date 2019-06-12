a961d6d74285d56d93abefdbd601856bb46cf5ec
Aruelius.L <25380989+Aruelius@users.noreply.github.com>
2019-06-12 14:36:07 +0800

    完全重写登录块
    完全摒弃selenium模拟登陆，使用全新requests登陆，现在二次验证支持邮箱跟手机验证码获取；优化了部分代码。

--- tieba.py
+++ tieba.py
@@ -1,108 +1,315 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding=utf-8 -*-
 import requests
-from selenium import webdriver
-from lxml import etree
-from PIL import Image
 import time
-import re
-import os
-import sys
-import json
-import threading
 import random
+import os.path
+import re
+import threading
+import json
+import rsa
+import base64
+from lxml import etree
 from io import BytesIO
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+from fake_useragent import UserAgent
+from urllib import parse
 from config import *
 
-def login(user, username, passwd, url = 'https://tieba.baidu.com/index.html'):
+AK = '1e3f2dd1c81f2075171a547893391274'
 
-	option = webdriver.ChromeOptions()
-	option.add_argument('--no-sandbox')
-	option.add_argument('--disable-extensions')
-	option.add_argument('--headless')
-	option.add_argument('lang=zh_CN.UTF-8')
-	option.add_argument('--disable-gpu')
-	option.add_argument('--disable-dev-shm-usage')
-	driver = webdriver.Chrome(executable_path='chromedriver', options=option)
-	driver.get(url)
-	time.sleep(0.16)
-	print('正在登录:%s...' % user)
-	driver.find_element_by_xpath('//*[@id="com_userbar"]/ul/li[4]/div/a').click() # 点击登录按钮
-	time.sleep(1)
-	driver.find_element_by_xpath('//*[@id="TANGRAM__PSP_10__footerULoginBtn"]').click() # 点击用户名登录
-	driver.find_element_by_xpath('//*[@id="TANGRAM__PSP_10__userName"]').send_keys(username) # 帐号
-	driver.find_element_by_xpath('//*[@id="TANGRAM__PSP_10__password"]').send_keys(passwd) # 帐号
-	driver.find_element_by_xpath('//*[@id="TANGRAM__PSP_10__submit"]').click() # 点击登录按钮
-	time.sleep(2.5)
-	try:
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__content_select_uname"]').click()
-		verification = 1 # 单二代验证
-	except:
-		print('尝试验证码 + 二代验证中...')
-		try:
-		    element = driver.find_element_by_id('TANGRAM__PSP_10__verifyCodeImg')
-		    verification = 2 # 验证码+二代验证
-		except:
-		    print('可能不需要验证码验证')
+def getTime():
+	return str(int(time.time() * 1000))
+
+def getGID():
+    def transform(char):
+        if char == "4" or char == "-": return char
+        number = random.randint(0, 15)
+        if char != "x": number = 3 & number | 8
+        return format(number, "x").upper()
+    return "".join([transform(c) for c in "xxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"])
+
+def getToken():
+	tt = getTime()
+
+	params = {
+		'tpl': 'tb',
+		'apiver': 'v3',
+		'tt': tt,
+		'class': 'login',
+		'gid': gid,
+	}
+
+	r = s.get(f'https://passport.baidu.com/v2/api/?getapi', params = params,
+					headers = {'User-Agent': ua}).text
+	token = ''.join(re.findall(' "token" : "(.+?)"', r))
+	return token
+
+def saveCookiesForUser(cookie):
+    ck_dict = requests.utils.dict_from_cookiejar(cookie)
+    with open('.%s' % user, 'w') as f:
+        ck_dict = str(ck_dict).replace("'", '"')
+        f.write(ck_dict)
+        f.close()
+
+def loadCookiesForUser():
+    with open('.%s' % user,'r') as fp1:
+        load_cookies = json.load(fp1)
+    return load_cookies
+
+def loginCheck():
+	tt = getTime()
+	params = {
+		'token': token,
+		'tpl': 'tb',
+		'apiver': 'v3',
+		'tt': tt,
+		'username': username,
+	}
+	r = s.get('https://passport.baidu.com/v2/api/?logincheck', params = params).json()
+	codeString, vcodetype = r['data']['codeString'], r['data']['vcodetype']
+
+	return codeString, vcodetype
+
+def getKey():
+	tt = getTime()
+
+	params = {
+		'token': token,
+		'tpl': 'tb',
+		'apiver': 'v3',
+		'tt': tt,
+		'gid': gid,
+	}
+
+	r = s.get('https://passport.baidu.com/v2/getpublickey', params = params, 
+					headers = {'User-Agent': ua}).text
+	resp = r.replace("'", '"')
+	pubkey = json.loads(resp)['pubkey']
+	key = json.loads(resp)['key']
+	return pubkey, key
+
+def getDSTK():
+	r = s.get(f'https://passport.baidu.com/viewlog?ak={AK}').json()
 	
-	if verification == 1:
-		time.sleep(1)
-		mode = input('需要二代验证,输入验证方式的序号\n1.手机验证\n2.密保邮箱验证\n请输入验证方式:')
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__select_email"]').click()
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__button_send_email"]').click()
-		vcode = input('请输入验证码:')
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__input_vcode"]').send_keys(vcode)
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__button_submit"]').click()
-		time.sleep(2.5)
-		cookies = driver.get_cookies()
-	elif verification == 2:
-		driver.save_screenshot("/tmp/codingpy.png")
-		time.sleep(3)
-		size = driver.find_element_by_id('TANGRAM__PSP_10__verifyCodeImg').size
-		left = int(element.location['x'])
-		top = int(element.location['y'])
-		right = int(element.location['x'] + element.size['width'])
-		bottom = int(element.location['y'] + element.size['height'])
-		im = Image.open('/tmp/codingpy.png')
-		im = im.crop((left, top, right, bottom))
-		im.save('code.png')
-		ucode = input('请打开code.png查看验证码\n验证码:')
-		driver.find_element_by_xpath('//*[@id="TANGRAM__PSP_10__verifyCode"]').send_keys(ucode) # 输入验证码
-		driver.find_element_by_xpath('//*[@id="TANGRAM__PSP_10__submit"]').click() # 点击登录按钮
-		time.sleep(3)
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__content_select_uname"]').click()
-		time.sleep(1)
-		mode = input('需要二代验证,输入验证方式的序号\n1.手机验证\n2.密保邮箱验证\n请输入验证方式:')
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__select_email"]').click()
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__button_send_email"]').click()
-		vcode = input('请输入验证码:')
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__input_vcode"]').send_keys(vcode)
-		driver.find_element_by_xpath('//*[@id="TANGRAM__25__button_submit"]').click()
-		time.sleep(2.5)
-		cookies = driver.get_cookies()
+	tk = r['data']['tk']
+	ds = r['data']['ds']
+	return tk, ds
+
+def getPassword():
+	pubkey, key = getKey()
+	en_pwd = base64.b64encode(
+		rsa.encrypt(
+			(password).encode('utf-8'),
+			rsa.PublicKey.load_pkcs1_openssl_pem(pubkey.encode())
+		)
+	)
+	pwd = bytes.decode(en_pwd)
+	return pwd, key
+
+def  loginReq(data):
+	r = s.post('https://passport.baidu.com/v2/api/?login', data = data, 
+					headers = {'User-Agent': ua})
+	resp = ''.join(re.findall('"err(.+?);', r.text))
+	return resp
+
+def getBDU(ltoken, lstr):
+	tt = getTime()
+	params = {
+		'u': 'https://tieba.baidu.com/index.html',
+		'tpl': 'tb',
+		'ltoken': ltoken,
+		'lstr': lstr,
+		'actiontype': '3',
+		'apiver': 'v3',
+		'tt': tt,
+	}
+	r = s.get('https://passport.baidu.com/v2/?loginproxy', params = params)
+	bdu = ''.join(re.findall('"hao123Param" : "(.+?)"', r.text))
+	return bdu
+
+def checkSMS(authtoken, lstr, ltoken, codeString, vcodetype):
+	vcode = input('手机验证码：')
+	params = {
+		'authtoken': authtoken,
+		'type': 'mobile',
+		'jsonp': '1',
+		'apiver': 'v3',
+		'action': 'check',
+		'vcode': vcode,
+		'u': 'https://tieba.baidu.com/index.html',
+		'lstr': lstr,
+		'ltoken': ltoken,
+		'tpl': 'tb',
+	}
+	r = s.get('https://passport.baidu.com/v2/sapi/authwidgetverify', params = params)
+	rsp = r.text.replace("'", '"')
+	json1 = json.loads(rsp)
+	
+	if json1['errno'] in ['110000']:
+		bdu = getBDU(ltoken, lstr)
+		tt = getTime()
+		r1 = s.get(f'https://user.hao123.com/static/crossdomain.php?bdu={bdu}&t={tt}')
+		r3 = s.get('http://tieba.baidu.com/f/like/mylike?pn=')
+		saveCookiesForUser(s.cookies)
+		s.cookies.clear()
+		s.headers.clear()
 	else:
-		print('未知错误！')
-		os.system("pkill chrome*")
-		sys.exit(0)
+		print(json1['msg'])
+		checkSMS(authtoken, lstr, ltoken, codeString, vcodetype)
 
+def checkMail(authtoken, lstr, ltoken, codeString, vcodetype):
+	vcode = input('邮件验证码:')
+	print(vcode)
+	params = {
+		'authtoken': authtoken,
+		'type': 'email',
+		'jsonp': '1',
+		'apiver': 'v3',
+		'action': 'check',
+		'vcode': vcode,
+		'u': 'https://tieba.baidu.com/index.html',
+		'lstr': lstr,
+		'ltoken': ltoken,
+		'tpl': 'tb',
+	}
 
-	print('登录成功!')
-	os.system("pkill chrome*")
-	return cookies
+	r = s.get('https://passport.baidu.com/v2/sapi/authwidgetverify', params = params)
+	rsp = r.text.replace("'", '"')
+	json1 = json.loads(rsp)
+	
+	if json1['errno'] in ['110000']:
+		bdu = getBDU(ltoken, lstr)
+		tt = getTime()
+		r1 = s.get(f'https://user.hao123.com/static/crossdomain.php?bdu={bdu}&t={tt}')
+		r3 = s.get('http://tieba.baidu.com/f/like/mylike?pn=') # 不访问一下这个会出毛病。。。
+		saveCookiesForUser(s.cookies)
+		s.cookies.clear()
+		s.headers.clear()
+	else:
+		print(json1['msg'])
+		checkMail(authtoken, lstr, ltoken, codeString, vcodetype)
 
-def gettbs(cookies, tieba):
+def SendSMS(authtoken, lstr, ltoken, codeString, vcodetype):
+	params = {
+		'authtoken': authtoken,
+		'type': 'mobile',
+		'jsonp': '1',
+		'action': 'send',
+		'u': 'https://tieba.baidu.com/index.html',
+		'lstr': lstr,
+		'ltoken': ltoken,
+		'tpl': 'tb',
+	}
+	r = s.get('https://passport.baidu.com/v2/sapi/authwidgetverify', params = params)
+	checkSMS(authtoken, lstr, ltoken, codeString, vcodetype)
 
-	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'}
-	r = requests.get('http://tieba.baidu.com/f?kw=%s' % tieba, headers = headers, cookies = cookies)
-	tbslist = re.findall("'tbs':'(.+?)'", r.text)
-	tbstr = "".join(tbslist)
-	return tbstr
+def SendMail(authtoken, lstr, ltoken, codeString, vcodetype):
+	params = {
+		'authtoken': authtoken,
+		'type': 'email',
+		'jsonp': '1',
+		'action': 'send',
+		'u': 'https://tieba.baidu.com/index.html',
+		'lstr': lstr,
+		'ltoken': ltoken,
+		'tpl': 'tb',
+	}
+	r = s.get('https://passport.baidu.com/v2/sapi/authwidgetverify', params = params)
+	checkMail(authtoken, lstr, ltoken, codeString, vcodetype)
+
+def TwoVerify(resp, codeString, vcodetype):
+	Resp  = parse.unquote_plus(resp)
+	authtoken = re.findall('authtoken=(.+?)&', Resp)[0]
+	ltoken = re.findall('ltoken=(.+?)&', Resp)[0]
+	lstr = re.findall('lstr=(.+?)&', Resp)[0]
+	lstr = parse.unquote_plus(lstr)
+
+	params = {
+		'authtoken': authtoken,
+		'jsonp': '1',
+		'apiver': 'v3',
+		'action': 'getapi',
+		'u': 'https://tieba.baidu.com/index.html',
+		'lstr': lstr,
+		'ltoken': ltoken,
+		'tpl': 'tb',
+	}
+
+	r = s.get('https://passport.baidu.com/v2/sapi/authwidgetverify', params = params,
+					headers = {'User-Agent': ua}).text.replace("'", '"').replace('|| ""', '')
+	json1 = json.loads(r)
+	
+	try:
+		if json1['msg'] == '系统繁忙，请稍后再试':
+			print(json1['msg'])
+			os._exit(0)
+		else:
+			verification = input('1.邮箱验证  2.手机验证\n请选择登录验证方式：')
+			if verification == '2':
+				mobile = json1['data']['mobile']
+				if mobile:
+					print(f'已经发送验证码给：{mobile}')
+					SendSMS(authtoken, lstr, ltoken, codeString, vcodetype)
+				else:
+					print('账户没有绑定手机号！')
+					os._exit(0)
+			elif verification == '1':
+				mail = json1['data']['email']
+				print(f'已经发送邮件给：{mail}')
+				SendMail(authtoken, lstr, ltoken, codeString, vcodetype)
+			else:
+				print('选择错误,退出...')
+				os._exit(0)
+	
+	except KeyboardInterrupt:
+		print('\n用户退出！')
+		os._exit(0)
+	except:
+		print('未知错误！') # 由于没遇到过没绑定邮箱跟手机号的账号，所以先未知错误
+		os._exit(0)
+
+def login():
+	print(f'正在登陆：{user}')
+	tt = getTime()
+	pwd, rsakey = getPassword()
+	tk, ds = getDSTK()
+	codeString, vcodetype = loginCheck()
+
+	data = {
+		'token': token,
+		'tpl': 'tb',
+		'apiver': 'v3',
+		'tt': tt,
+		'u': 'https://tieba.baidu.com/index.html',
+		'gid': gid,
+		'username': username,
+		'password': pwd,
+		'rsakey': rsakey,
+		'crypttype': '12',
+		'ds': ds,
+		'tk': tk
+	}
+
+	if codeString:
+		print('需要验证码,请打开下方链接查看')
+		print(f'https://passport.baidu.com/cgi-bin/genimage?{codeString}')
+		
+		verifycode = input('验证码：')
+		data['codestring'] = codeString
+		data['verifycode'] = verifycode
+		data['vcodefrom'] = 'checkuname'
+		resp = loginReq(data)
+		
+		TwoVerify(resp, codeString, vcodetype) # 二次验证
+	else:
+		resp = loginReq(data)
+		TwoVerify(resp, codeString, vcodetype)
 
 def getbar(cookies):
-
 	url = 'http://tieba.baidu.com/f/like/mylike?pn='
-	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'}
-	r_bar = requests.get(url + '1', headers = headers, cookies = cookies)
+	r_bar = requests.get(url + '1', cookies = cookies)
 	webpage = etree.HTML(r_bar.text)
 
 	pages = ['1']
@@ -113,42 +320,41 @@ def getbar(cookies):
 
 	tiebas = []
 	for pageid in pages:
-		r_bar_1 = requests.get(url + pageid, headers = headers, cookies = cookies)
+		r_bar_1 = requests.get(url + pageid, cookies = cookies)
 		webpage = etree.HTML(r_bar_1.text)
 		tag_a = webpage.xpath('//a[@title]') # 获取贴吧
 		for a in tag_a:
 			if a.text:
 				tiebas.append(a.text)
-
 	return tiebas
 
 def recognize_captcha(remote_url, rec_times):
+	headers = {
+	    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/\
+	    		537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"
+	}
 
-    headers = {
-        'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"
-    }
+	for index in range(rec_times):
+	    # 请求
+	    while True:
+	        try:
+	            response = requests.request("GET", remote_url, headers=headers, timeout=6)
+	            if response.text:
+	                break
+	            else:
+	                print("retry, response.text is empty")
+	        except Exception as ee:
+	            print(ee)
 
-    for index in range(rec_times):
-        # 请求
-        while True:
-            try:
-                response = requests.request("GET", remote_url, headers=headers, timeout=6)
-                if response.text:
-                    break
-                else:
-                    print("retry, response.text is empty")
-            except Exception as ee:
-                print(ee)
+	    # 识别
+	    url = "http://1i1.tw:10086/b"
+	    files = {'image_file': ('captcha.jpg', BytesIO(response.content), 'application')}
+	    r = requests.post(url=url, files=files)
 
-        # 识别
-        url = "http://1i1.tw:10086/b"
-        files = {'image_file': ('captcha.jpg', BytesIO(response.content), 'application')}
-        r = requests.post(url=url, files=files)
+	    # 识别结果
+	    predict_text = json.loads(r.text)["value"]
 
-        # 识别结果
-        predict_text = json.loads(r.text)["value"]
-
-        return predict_text
+	    return predict_text
 
 def sign_vcode(tieba, tbs, captcha_input_str, captcha_vcode_str, cookies, headers):
 
@@ -159,7 +365,8 @@ def sign_vcode(tieba, tbs, captcha_input_str, captcha_vcode_str, cookies, header
 		'captcha_input_str':captcha_input_str,
 		'captcha_vcode_str':captcha_vcode_str
 	}
-	r = requests.post('http://tieba.baidu.com/sign/add', headers = headers, data = data, cookies = cookies)
+	r = requests.post('http://tieba.baidu.com/sign/add', headers = headers, 
+						data = data, cookies = cookies)
 	try:
 		resp = r.json()
 		if resp['data']['errmsg'] == 'success':
@@ -167,33 +374,30 @@ def sign_vcode(tieba, tbs, captcha_input_str, captcha_vcode_str, cookies, header
 			print('状态:打码签到成功!')
 
 	except:
-		resp = r.json()
 		print('贴吧:%s\t' % tieba, end = '')
 		print('状态:%s' % resp['error'])
 
-def sign(cookies, tieba):
+def gettbs(cookies):
+	r = requests.get('http://tieba.baidu.com/dc/common/tbs', cookies = cookies).json()
+	return r['tbs']
 
-	tbs = gettbs(cookies, tieba)
+def sign(cookies, tieba):
+	tbs = gettbs(cookies)
 	headers = {
 		'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
 		'Origin':'http://tieba.baidu.com',
 		'Referer':'http://tieba.baidu.com',
-		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
+		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWeb\
+		Kit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
 	}
-
-	"""
-	data = {
-		'ie':'utf-8',
-		'tbs':tbs, # 一键签到 只能签到规定数量的贴吧
-	}
-	"""
 
 	data = {
 		'ie':'utf-8',
 		'kw':tieba,
-		'tbs':tbs # 分开签到
+		'tbs':tbs
 	}
-	r = requests.post('http://tieba.baidu.com/sign/add', headers = headers, data = data, cookies = cookies)
+	r = requests.post('http://tieba.baidu.com/sign/add', headers = headers, 
+						data = data, cookies = cookies)
 	try:
 		resp = r.json()
 		if resp['data']['errmsg'] == 'success':
@@ -201,7 +405,6 @@ def sign(cookies, tieba):
 			print('状态:签到成功!')
 	
 	except:
-		resp = r.json()
 		if resp['error'] == 'need vcode': # 需要验证码
 			captcha_vcode_str = resp['data']['captcha_vcode_str']
 			captcha_url = 'https://tieba.baidu.com/cgi-bin/genimg?%s' % captcha_vcode_str
@@ -211,69 +414,61 @@ def sign(cookies, tieba):
 			print('贴吧:%s\t' % tieba, end = '') # 黑名单或者别的情况
 			print('状态:%s' % resp['error'])
 
-def login_cookies(user, username, passwd):
-
-	cookie = login(user, username, passwd)
-	for item in cookie:
-		s.cookies.set(item['name'],item['value'])
-	cookies = requests.utils.dict_from_cookiejar(s.cookies)
-	with open('.%s' % user,'w') as fp:
-		json.dump(cookies,fp)
-		fp.close()
-	tiebas = getbar(cookies)
-	list.extend(tiebas)
-
-	threads=[]
-	for i in range(len(tiebas)): # 多少个贴吧就分配多少个线程
-		t=threading.Thread(target=sign,args=(cookies, tiebas[0+i]))
+def Start(tiebas, cookies):
+	threads=[] 
+	for i in tiebas:
+		t=threading.Thread(target=sign,args=(cookies, i))
 		threads.append(t)
 	
 	for i in threads:
 		i.start()
-
+	
 	for i in threads:
 		i.join()
-	
-	s.cookies.clear()
-	os.system("pkill chrome*")
 
 def main():
-	for x in range(len(users)):
-		user = users[x]
+	global user, username, password, s, ua, gid, token, mailuser, mailpwd
+	ua = UserAgent(verify_ssl=False).random
+	for user in users:
 		username = accounts[user]['username']
-		passwd = accounts[user]['password']
+		password = accounts[user]['password']
+		mailuser = accounts[user]['mailuser']
+		mailpwd = accounts[user]['mailpwd']
+		s = requests.session()
+		r = s.get('https://tieba.baidu.com')
 		if os.path.exists('.%s' % user):
-			with open('.%s' % user,'r') as fp1:
-				load_cookies = json.load(fp1)
+			load_cookies = loadCookiesForUser()
 			cookies = requests.utils.cookiejar_from_dict(load_cookies)
-			print('正在使用Cookies登录:%s...' % user)
-			r = requests.get('http://tieba.baidu.com/sysmsg/query/userunread', cookies = cookies)
-			resp = r.json()
-			if resp['errmsg'] == '成功':
+
+			print(f'正在使用Cookies登陆:{user}')
+			r = s.get('http://tieba.baidu.com/sysmsg/query/userunread', 
+						cookies = cookies).json()
+			if r['errmsg'] == '成功':
 				tiebas = getbar(cookies)
 				list.extend(tiebas)
-
-				threads=[] 
-				for i in range(len(tiebas)):
-					t=threading.Thread(target=sign,args=(cookies, tiebas[0+i]))
-					threads.append(t)
-				
-				for i in threads:
-					i.start()
-				
-				for i in threads:
-					i.join()
-
-				os.system("pkill chrome*")
+				Start(tiebas, cookies)
 			else:
 				print('%sCookies失效...正在重新登录...' % user)
-				login_cookies(user, username, passwd)
+				gid = getGID()
+				token = getToken()
+				login()
+				load_cookies = loadCookiesForUser()
+				cookies = requests.utils.cookiejar_from_dict(load_cookies)
+				tiebas = getbar(cookies)
+				list.extend(tiebas)
+				Start(tiebas, cookies)
 		else:
-			login_cookies(user, username, passwd)
+			gid = getGID()
+			token = getToken()
+			login()
+			load_cookies = loadCookiesForUser()
+			cookies = requests.utils.cookiejar_from_dict(load_cookies)
+			tiebas = getbar(cookies)
+			list.extend(tiebas)
+			Start(tiebas, cookies)
 
 if __name__ == '__main__':
 	list = []
-	s = requests.session()
 	start = time.time()
 	main()
 	end = time.time()
