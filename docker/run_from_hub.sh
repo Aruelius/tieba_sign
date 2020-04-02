7b59516c7bb57f651354c33676f8afd0e58162a7
jianshli <wxs-studio@cisco.com>
2020-04-02 16:18:52 +0800

    [mod]move build dependency to _build folder

--- docker/run_from_hub.sh
+++ docker/run_from_hub.sh
@@ -4,11 +4,12 @@ print_usage () {
     echo ""
     echo "------------------USAGE------------------------"
     echo "COMMAND:"
-    echo "./run.sh mode"
+    echo "./run_from_hub.sh mode"
     echo ""
     echo "PARAMS:"
     echo "mode: 0=minimize, 1=full_funtion"
     echo "------------------USAGE------------------------"
+    # shellcheck disable=SC2242
     exit -1
 }
 
@@ -18,12 +19,12 @@ fi
 
 if [ ${1} -eq 0 ]
 then
-    check_results=`docker run -it -d --name tieba_sign_min tieba_sign_min:latest`
-    docker logs -f $check_results
+    check_results=$(docker run -it -d --name tieba_sign_min ck123pm/tieba_sign_min)
+    docker logs -f "$check_results"
 elif [ ${1} -eq 1 ]
 then
-    check_results=`docker run -it --name tieba_sign_full tieba_sign_full:latest`
-    docker logs -f $check_results
+    check_results=$(docker run -it --name tieba_sign_full ck123pm/tieba_sign_full)
+    docker logs -f "$check_results"
 else
     echo "invalid mode: ${1}"
     exit -1
